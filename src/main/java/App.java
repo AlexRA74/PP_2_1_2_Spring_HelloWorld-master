9a5959ced01b113a61e0f2a8111bc4edf2ffa3d5
Алексей Рузин <ruzun@mail.ru>
2023-06-13 22:37:47 +0500

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+        System.out.println("Переменные bean1 и bean2 ссылаются на один и тот же объект? " + (bean1==bean2));
+        System.out.println("Переменные beanCat1 и beanCat2 ссылаются на один и тот же объект? " + (beanCat1==beanCat2));
+    }
+}
